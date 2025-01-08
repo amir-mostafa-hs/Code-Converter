f8b86c0b9e6648bec27d2a956dfb68f921ac65d9
Amir Mostafa Haji Sadeghian <amir-mostafa-hs@outlook.com>
2025-01-08 15:54:25 +0330

    config electron production

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // حالت استاتیک
+  assetPrefix: "/", // تنظیم مسیر فایل‌ها برای Electron
+  trailingSlash: true, // اطمینان از اینکه مسیرها به درستی مشخص هستند
+};
 
 export default nextConfig;
