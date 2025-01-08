a5387bdf782997acfdcb7b53f3dd186f169c841e
Amir Mostafa Haji Sadeghian <amir-mostafa-hs@outlook.com>
2025-01-08 15:53:17 +0330

    add sample code

---
+++ SimpleCode/badFormatCode.js
@@ -0,0 +1,20 @@
+function myFunction( a ,b) {
+if(a> b){
+return a
+;
+}else{
+return
+b;
+}
+}
+let myVariable = 10;
+const   myConstant  =   "hello";
+for(let i=0;i< 10;i++){
+console.log(i );
+}
+const myObject = {
+name:"John",age:30,
+city:"New York"
+};
+console.log(myFunction(5,12));
+console.log(myObject);
